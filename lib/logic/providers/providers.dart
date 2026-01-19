4d48a785534a0ccd9dc7ab50a4352e2854c9d63b
joaoVR-prado <joaorodriguesprado17@gmail.com>
2026-01-18 22:09:08 -0300

    feat: configura provider

---
+++ lib/logic/providers/providers.dart
@@ -0,0 +1,18 @@
+
+import 'package:hive_ce/hive.dart';
+import 'package:make_a_habbit/data/models/concluded_habits/concluded_habits_model.dart';
+import 'package:make_a_habbit/data/models/habits/habit_model.dart';
+import 'package:make_a_habbit/data/repositories/habit_repository.dart';
+import 'package:riverpod/riverpod.dart';
+
+final HabitRepositoryProvider = Provider<IHabitRepository>((ref) {
+  final habitBox = Hive.box<HabitModel>('habits');
+  final conclusionBox = Hive.box<ConcludedHabitsModel>('conclusions');
+
+  return HabitRepository(
+    habitBox, 
+    conclusionBox
+
+  );
+
+});
