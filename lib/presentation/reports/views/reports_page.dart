8856f0d39c2f412a260588775db71a8b8504d79d
joaoVR-prado <joaorodriguesprado17@gmail.com>
2026-05-31 18:57:45 -0300

    feat: Cria esqueleto da tela de relatório e monta o provider

---
+++ lib/presentation/reports/views/reports_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:make_a_habbit/core/theme/app_colors.dart';
+import 'package:make_a_habbit/data/providers/habit_stats_provider.dart';
+import 'package:make_a_habbit/presentation/reports/widgets/weekly_graphic_card.dart';
+
+class ReportsPage extends ConsumerWidget {
+  const ReportsPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref){
+    final stats = ref.watch(habitStatsProvider);
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              Text('Seu Progresso Geral: ', style: Theme.of(context).textTheme.titleLarge),
+              const SizedBox(height: 10),
+              
+              WeeklyGraphicCard(weeklyData: stats.weeklyCompletionHistory), 
+              const SizedBox(height: 10),
+              
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                children: [
+                  _buildStatCard(context, 'Taxa de Sucesso', '${stats.generalSuccessRate.toStringAsFixed(1)}%', Icons.bolt),
+                  _buildStatCard(context, 'Recorde de Ofensiva', '${stats.bestStreakGeral} dias', Icons.local_fire_department),
+                  _buildStatCard(context, 'Total de Hábitos', '${stats.totalHabits}', Icons.assignment),
+                  _buildStatCard(context, 'Concluídos Hoje', '${stats.completedToday}', Icons.check_circle),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStatCard(BuildContext context, String title, String value, IconData icon) {
+    return Card(
+      color: AppColors.cardBackgrounColor,
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 32, color: AppColors.positiveActionDialogTextColor),
+            const SizedBox(height: 8),
+            Text(value, style: Theme.of(context).textTheme.headlineMedium),
+            const SizedBox(height: 4),
+            Text(title, style: Theme.of(context).textTheme.labelSmall, textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
